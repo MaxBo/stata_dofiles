7327476796512062efa84dd037c51e56006dca8f
mBohnet <bohnet@ggr-planung.de>
2011-10-14 01:05:53 +0200

    label var, label define, generate werden übersetzt

--- MZ/SPSS2Stata.py
+++ MZ/SPSS2Stata.py
@@ -12,6 +12,10 @@
 
 path = r'D:\DatenMaxDiss\ado\dofiles\MZ\anne.sps'
 
+import re
+p_into = re.compile('(?<=INTO )\w+')
+p_labels = re.compile('(?<=LABELS )\w+')
+pw = re.compile("\s{2,}")
 def convert():
     f = open(path)
     try:
@@ -21,7 +25,7 @@ def convert():
                 if z.startswith("RECODE"):
                     command = 'recode '
                     link = False
-                    while not z.startswith("EXECUTE"):
+                    while True:
                         value = z.strip().replace('RECODE','')
                         if link:
                             value = value[1:]
@@ -32,8 +36,46 @@ def convert():
                             link = False
                         value = value.replace ("thru","/")
                         command += value
+                        if z.strip().endswith('.'):
+                            command = command.rstrip('.')
+                            break
                         z = f.readline()
+                    command = command.replace('ELSE','else')
+                    command = re.sub(pw,' ',command)
+
+                    m = p_into.search(command)
+                    if m:
+                        newVarName = m.group(0)
+##                        print newVarName
+                        command = command.replace('INTO %s' %newVarName, ', generate(%s)' %newVarName)
                     print command
+
+
+                if z.startswith("VARIABLE LABELS"):
+                    command = z.strip().replace("VARIABLE LABELS", "label variable").rstrip('.')
+                    print command
+
+                if z.startswith("VALUE LABELS"):
+                    command = z.strip().replace("VALUE LABELS", "label define").rstrip('.')
+                    command = command.replace('_$','')
+                    print command
+                    command2 = z.strip().rstrip('.')
+                    command2 = re.sub(pw,' ',command2).replace('_$','')
+                    m = p_labels.search(command2)
+                    if m:
+                        varName = m.group(0)
+##                        print varName
+                        command2 = 'label values %(vn)s %(vn)s' %{'vn': varName}
+                        print command2
+
+
+                if z.startswith("COMPUTE"):
+                    command = z.strip().replace("COMPUTE", "generate").rstrip('.')
+                    command = command.replace('_$','')
+                    # to do: = durch == ersetzen
+                    print command
+
+
             z = f.readline()
     finally:
         f.close()
